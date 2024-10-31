c2e3c45bd4a667698b065bc4ec440fd0a329e1f3
Thibault Dausque <tdausque@3B3.42angouleme.fr>
2024-10-31 17:01:23 +0100

    first message

---
+++ get_next_line_utils.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdausque <tdausque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 15:24:15 by tdausque          #+#    #+#             */
+/*   Updated: 2024/10/31 13:15:12 by tdausque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (unsigned char)c && *s != '\0')
+		s++;
+	if (*s == (unsigned char)c)
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		size;
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!src)
+		return (0);
+	size = ft_strlen(src);
+	dest = (char *)malloc((size + 1) * sizeof(char));
+	if (!dest)
+		return (0);
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	char	*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(i * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str[ft_strlen(s1) + i] = s2[i];
+		i++;
+	}
+	str[ft_strlen(s1) + i] = '\0';
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = start;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	while (s[j] != '\0' && i < len)
+	{
+		str[i] = s[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
